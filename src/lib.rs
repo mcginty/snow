696568b88a5198b2e2fb3232002b99573d8246c2
trevp <github@trevp.net>
2015-10-19 17:24:55 -0700

    Start adding testing

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ extern crate rand;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use crypto::hmac::Hmac;
-use crypto::mac::{Mac, MacResult};
+use crypto::mac::{Mac};
 use crypto::aes::KeySize;
 use crypto::aes_gcm::AesGcm;
 use crypto::aead::{AeadEncryptor, AeadDecryptor};
@@ -16,7 +16,7 @@ use rand::{OsRng, Rng};
 
 const DHLEN : usize = 32;
 const HASHLEN : usize = 32;
-const BLOCKLEN : usize = 64;
+//const BLOCKLEN : usize = 64;
 const MACLEN : usize = 16;
 const CIPHERKEYLEN : usize = 32;
 
@@ -25,12 +25,12 @@ fn copy_memory(data: &[u8], out: &mut [u8]) {
 }
 
 
-struct KeyPair {
+pub struct KeyPair {
     privkey: [u8; DHLEN],
     pubkey: [u8; DHLEN]
 }
 
-fn GENERATE_KEYPAIR() -> KeyPair {
+pub fn generate_keypair() -> KeyPair {
     let mut privkey = [0u8; 32];
     let mut rng = OsRng::new().unwrap();
     rng.fill_bytes(&mut privkey);
@@ -38,35 +38,35 @@ fn GENERATE_KEYPAIR() -> KeyPair {
     KeyPair{privkey : privkey, pubkey : pubkey}
 }
 
-fn DH(keypair : &KeyPair, pubkey: &[u8]) -> [u8; DHLEN] {
+fn dh(keypair : &KeyPair, pubkey: &[u8]) -> [u8; DHLEN] {
     curve25519(&keypair.privkey, pubkey)
 }
 
 
-fn HASH(data: &[u8], out: &mut [u8]) {
-    let mut hash = Sha256::new();
-    hash.input(data);
-    hash.result(out);
+pub fn hash(data: &[u8], out: &mut [u8]) {
+    let mut h = Sha256::new();
+    h.input(data);
+    h.result(out);
 }
 
-fn HMAC_HASH(key: &[u8], data: &[u8], out: &mut [u8]) {
+fn hmac_hash(key: &[u8], data: &[u8], out: &mut [u8]) {
     let mut hmac = Hmac::new(Sha256::new(), key);
     hmac.input(data);
     hmac.raw_result(out);
 }
 
-fn HKDF(chaining_key: &[u8], data: &[u8], out: &mut [u8]) {
+fn hkdf(chaining_key: &[u8], data: &[u8], out: &mut [u8]) {
     let mut temp_key = [0u8; HASHLEN];
     let mut in2  = [0u8; HASHLEN];
-    HMAC_HASH(chaining_key, data, &mut temp_key);
-    HMAC_HASH(&temp_key, &[1u8], &mut out[0..HASHLEN]);
+    hmac_hash(chaining_key, data, &mut temp_key);
+    hmac_hash(&temp_key, &[1u8], &mut out[0..HASHLEN]);
     copy_memory(out as &[u8], &mut in2);
     in2[HASHLEN] = 2;
-    HMAC_HASH(&temp_key, &in2, &mut out[HASHLEN..2*HASHLEN]);
+    hmac_hash(&temp_key, &in2, &mut out[HASHLEN..2*HASHLEN]);
 }
 
 
-fn ENCRYPT(key: &[u8], nonce: u64, authtext: &[u8], plaintext: &[u8], out: &mut[u8]) {
+fn encrypt(key: &[u8], nonce: u64, authtext: &[u8], plaintext: &[u8], out: &mut[u8]) {
     let mut nonce_bytes = [0u8; 12];
     BigEndian::write_u64(&mut nonce_bytes[4..], nonce);
     let mut cipher = AesGcm::new(KeySize::KeySize256, key, &nonce_bytes, authtext);
@@ -75,19 +75,19 @@ fn ENCRYPT(key: &[u8], nonce: u64, authtext: &[u8], plaintext: &[u8], out: &mut[
     copy_memory(&tag, &mut out[plaintext.len()..]);
 } 
 
-fn DECRYPT(key: &[u8], nonce: u64, authtext: &[u8], ciphertext: &[u8], out: &mut[u8]) -> bool {
+fn decrypt(key: &[u8], nonce: u64, authtext: &[u8], ciphertext: &[u8], out: &mut[u8]) -> bool {
     let mut nonce_bytes = [0u8; 12];
     BigEndian::write_u64(&mut nonce_bytes[4..], nonce);
     let mut cipher = AesGcm::new(KeySize::KeySize256, key, &nonce_bytes, authtext);
-    let mut tag = [0u8; MACLEN];
+    let tag = [0u8; MACLEN];
     cipher.decrypt(ciphertext, out, &tag)
 } 
 
-enum Token {e, s, dhee, dhes, dhse, dhss}
+pub enum Token {E, S, Dhee, Dhes, Dhse, Dhss}
 
-enum NoiseError {DecryptError}
+pub enum NoiseError {DecryptError}
 
-struct CipherState {
+pub struct CipherState {
     k: [u8; CIPHERKEYLEN],
     n: u64
 }
@@ -99,7 +99,7 @@ struct SymmetricState {
     h: [u8; HASHLEN]
 }
 
-struct HandshakeState {
+pub struct HandshakeState {
     symmetricstate: SymmetricState,
     s: KeyPair,
     e: KeyPair,
@@ -114,13 +114,13 @@ impl CipherState {
         CipherState{k : [0; CIPHERKEYLEN], n:0}
     }
 
-    fn encrypt_and_increment(&mut self, authtext: &[u8], plaintext: &[u8], out: &mut [u8]) {
-        ENCRYPT(&self.k, self.n, authtext, plaintext, out);
+    pub fn encrypt_and_increment(&mut self, authtext: &[u8], plaintext: &[u8], out: &mut [u8]) {
+        encrypt(&self.k, self.n, authtext, plaintext, out);
         self.n += 1;
     }
 
-    fn decrypt_and_increment(&mut self, authtext: &[u8], ciphertext: &[u8], out: &mut [u8]) -> bool {
-        if !DECRYPT(&self.k, self.n, authtext, ciphertext, out) {
+    pub fn decrypt_and_increment(&mut self, authtext: &[u8], ciphertext: &[u8], out: &mut [u8]) -> bool {
+        if !decrypt(&self.k, self.n, authtext, ciphertext, out) {
             self.n += 1;
             return false;
         }
@@ -135,7 +135,7 @@ impl SymmetricState {
         let mut hname = [0u8; HASHLEN];
         match handshake_name.len() {
             0 ... HASHLEN => copy_memory(handshake_name, &mut hname),
-            _ => HASH(handshake_name, &mut hname)
+            _ => hash(handshake_name, &mut hname)
         }
         let cipherstate = CipherState::new();
         SymmetricState{cipherstate: cipherstate, has_key : false, ck : hname, h: hname}
@@ -143,7 +143,7 @@ impl SymmetricState {
 
     fn mix_key(&mut self, data: &[u8]) {
         let mut hkdf_output = [0u8; 2*HASHLEN];
-        HKDF(&self.ck, data, &mut hkdf_output);
+        hkdf(&self.ck, data, &mut hkdf_output);
         copy_memory(&hkdf_output[0..HASHLEN], &mut self.ck);
         copy_memory(&hkdf_output[HASHLEN..HASHLEN+CIPHERKEYLEN], &mut self.cipherstate.k);
         self.cipherstate.n = 0;
@@ -154,7 +154,7 @@ impl SymmetricState {
         let mut buffer = [0u8; 65535];
         copy_memory(&data, &mut buffer);
         copy_memory(&self.h, &mut buffer[data.len()..data.len() + HASHLEN]);
-        HASH(&buffer, &mut self.h);
+        hash(&buffer, &mut self.h);
     }
 
     fn encrypt_and_hash(&mut self, plaintext: &[u8], out: &mut [u8]) -> usize {
@@ -184,11 +184,11 @@ impl SymmetricState {
 
     fn split(&self) -> (CipherState, CipherState) {
         let mut hkdf_output = [0u8; 2*HASHLEN];
-        HKDF(&self.ck, &[0u8; 0], &mut hkdf_output);
+        hkdf(&self.ck, &[0u8; 0], &mut hkdf_output);
         let mut c1 = CipherState::new();
         let mut c2 = CipherState::new();
         copy_memory( &hkdf_output[0..CIPHERKEYLEN], &mut c1.k);
-        copy_memory( &hkdf_output[HASHLEN..HASHLEN+CIPHERKEYLEN], &mut c1.k);
+        copy_memory( &hkdf_output[HASHLEN..HASHLEN+CIPHERKEYLEN], &mut c2.k);
         (c1, c2)
     }
 
@@ -196,28 +196,28 @@ impl SymmetricState {
 
 impl HandshakeState {
 
-    fn new(handshake_name: &[u8], new_s : KeyPair, new_e : KeyPair, new_rs: [u8; 32], new_re : [u8; 32] ) -> HandshakeState {
+    pub fn new(handshake_name: &[u8], new_s : KeyPair, new_e : KeyPair, new_rs: [u8; 32], new_re : [u8; 32] ) -> HandshakeState {
         let symmetricstate = SymmetricState::new(handshake_name); 
         HandshakeState{symmetricstate: symmetricstate, s: new_s, e: new_e, rs: new_rs, re: new_re}
     }
 
-    fn write_handshake_message(&mut self, 
+    pub fn write_handshake_message(&mut self, 
                                descriptor: &[Token], 
                                last: bool, 
                                payload: &[u8], 
                                out: &mut [u8]) -> Option<(CipherState, CipherState)> { 
         let mut index = 0;
         for token in descriptor {
-            match (*token) {
-                Token::e => {
-                    self.e = GENERATE_KEYPAIR(); 
+            match *token {
+                Token::E => {
+                    self.e = generate_keypair(); 
                     index += self.symmetricstate.encrypt_and_hash(&self.e.pubkey, &mut out[index..]); 
                 },
-                Token::s => index += self.symmetricstate.encrypt_and_hash(&self.s.pubkey, &mut out[index..]),
-                Token::dhee => self.symmetricstate.mix_key(&DH(&self.e, &self.re)),
-                Token::dhes => self.symmetricstate.mix_key(&DH(&self.e, &self.rs)),
-                Token::dhse => self.symmetricstate.mix_key(&DH(&self.s, &self.re)),
-                Token::dhss => self.symmetricstate.mix_key(&DH(&self.s, &self.rs)),
+                Token::S => index += self.symmetricstate.encrypt_and_hash(&self.s.pubkey, &mut out[index..]),
+                Token::Dhee => self.symmetricstate.mix_key(&dh(&self.e, &self.re)),
+                Token::Dhes => self.symmetricstate.mix_key(&dh(&self.e, &self.rs)),
+                Token::Dhse => self.symmetricstate.mix_key(&dh(&self.s, &self.re)),
+                Token::Dhss => self.symmetricstate.mix_key(&dh(&self.s, &self.rs)),
             }
         }
         self.symmetricstate.encrypt_and_hash(payload, &mut out[index..]);
@@ -227,30 +227,30 @@ impl HandshakeState {
         }
     }
 
-    fn read_handshake_message(&mut self, 
+    pub fn read_handshake_message(&mut self, 
                               buffer: &[u8], 
                               descriptor: &[Token], 
                               last: bool, 
                               out: &mut [u8]) -> Result<Option<(CipherState, CipherState)>, NoiseError> { 
         let mut ptr = buffer;
         for token in descriptor {
-            match (*token) {
-                Token::e | Token::s => {
+            match *token {
+                Token::E | Token::S => {
                     let data = match self.symmetricstate.has_key {
                         true =>  {let temp = &ptr[..DHLEN+16]; ptr = &ptr[DHLEN+16..]; temp}
                         false => {let temp = &ptr[..DHLEN];    ptr = &ptr[DHLEN..];    temp}
                     };
-                    if !self.symmetricstate.decrypt_and_hash(data, match (*token) {
-                            Token::e => &mut self.re,
-                            Token::s => &mut self.rs,
+                    if !self.symmetricstate.decrypt_and_hash(data, match *token {
+                            Token::E => &mut self.re,
+                            Token::S => &mut self.rs,
                             _ => unreachable!()}) {
                         return Err(NoiseError::DecryptError);
                     }
                 },
-                Token::dhee => self.symmetricstate.mix_key(&DH(&self.e, &self.re)),
-                Token::dhes => self.symmetricstate.mix_key(&DH(&self.s, &self.re)),
-                Token::dhse => self.symmetricstate.mix_key(&DH(&self.e, &self.rs)),
-                Token::dhss => self.symmetricstate.mix_key(&DH(&self.s, &self.rs)),
+                Token::Dhee => self.symmetricstate.mix_key(&dh(&self.e, &self.re)),
+                Token::Dhes => self.symmetricstate.mix_key(&dh(&self.s, &self.re)),
+                Token::Dhse => self.symmetricstate.mix_key(&dh(&self.e, &self.rs)),
+                Token::Dhss => self.symmetricstate.mix_key(&dh(&self.s, &self.rs)),
             }
         }
         if !self.symmetricstate.decrypt_and_hash(ptr, out) {
@@ -265,3 +265,24 @@ impl HandshakeState {
 }
 
 
+
+
+#[cfg(test)]
+mod tests {
+
+    extern crate rustc_serialize;
+
+    use super::*;
+    use self::rustc_serialize::hex::ToHex;
+
+    #[test]
+    fn crypto_tests() {
+
+        // SHA256 test
+        let mut output = [0u8; 32];
+        HASH("abc".as_bytes(), &mut output);
+        assert!(output.to_hex() == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
+
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 extern crate noiseref;
 
 fn main() {
-    let mut output = [0u8; 32];
-    noiseref::HASH("abc".as_bytes(), &mut output);
-    for b in &output {print!("{:x}", b);}
 }
