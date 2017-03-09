16c12457ad45bdc0949519263919153ad4146c7e
Jake McGinty <me@jake.su>
2017-03-10 04:02:10 +0800

    cleaner comments for example

--- examples/simple.rs
+++ examples/simple.rs
@@ -15,6 +15,7 @@ use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let matches = App::new("simple").args_from_usage("-s --server 'Server mode'").get_matches();
+
     let params = "Noise_NN_25519_ChaChaPoly_BLAKE2s".parse().unwrap();
 
     if matches.is_present("server") {
@@ -26,49 +27,57 @@ fn main() {
 }
 
 fn run_server(params: NoiseParams) {
-    let mut noise = NoiseBuilder::new(params).build_responder().unwrap();
     let mut buf = vec![0u8; 65535];
 
+    // Initialize our responder NoiseSession using a builder.
+    let mut noise = NoiseBuilder::new(params).build_responder().unwrap();
+
+    // Wait on our client's arrival...
     println!("listening on 127.0.0.1:9999");
     let (mut stream, _) = TcpListener::bind("127.0.0.1:9999").unwrap().accept().unwrap();
 
-    // get first message from intiator
+    // We're acting as the responder, so the first message is sent from the client.
     noise.read_message(&recv(&mut stream), &mut buf).unwrap();
 
-    // respond, completing the handshake for NN
+    // Respond, thereby completing the handshake for NN, which is 1RTT.
     let len = noise.write_message(&[0u8; 0], &mut buf).unwrap();
     send(&mut stream, &buf[..len]);
 
-    // transition the state machine into transport mode
+    // Transition the state machine into transport mode now that the handshake is complete.
     let mut noise = noise.into_transport_mode().unwrap();
-    println!("session established...");
 
     let len = noise.read_message(&recv(&mut stream), &mut buf).unwrap();
     println!("client said: {}", String::from_utf8_lossy(&buf[..len]));
 }
 
 fn run_client(params: NoiseParams) {
-    let mut noise = NoiseBuilder::new(params).build_initiator().unwrap();
     let mut buf = vec![0u8; 65535];
+
+    // Initialize our initiator NoiseSession using a builder.
+    let mut noise = NoiseBuilder::new(params).build_initiator().unwrap();
+
+    // Connect to our server, which is hopefully listening.
     let mut stream = TcpStream::connect("127.0.0.1:9999").unwrap();
     println!("connected...");
 
-    // initiator sends the first message
+    // We're the initiator, so we're responsible for sending the first message.
     let len = noise.write_message(&[0u8; 0], &mut buf).unwrap();
     send(&mut stream, &buf[..len]);
 
-    // get response, completing the handshake for NN
+    // Receive response, thereby completing the handshake for NN
     noise.read_message(&recv(&mut stream), &mut buf).unwrap();
 
-    // transition the state machine into transport mode
+    // Transition the state machine into transport mode now that the handshake is complete.
     let mut noise = noise.into_transport_mode().unwrap();
     println!("session established...");
 
+    // Get to the important business of sending secured data.
     let len = noise.write_message("HACK THE PLANET".as_bytes(), &mut buf).unwrap();
     send(&mut stream, &buf[..len]);
     println!("notified server of intent to hack planet.");
 }
 
+/// Hyper-basic stream transort receiver. 16-bit BE size followed by payload.
 fn recv(stream: &mut TcpStream) -> Vec<u8> {
     let mut msg_len_buf = [0u8; 2];
     stream.read_exact(&mut msg_len_buf).unwrap();
@@ -78,6 +87,7 @@ fn recv(stream: &mut TcpStream) -> Vec<u8> {
     msg
 }
 
+/// Hyper-basic stream transort sender. 16-bit BE size followed by payload.
 fn send(stream: &mut TcpStream, buf: &[u8]) {
     let mut msg_len_buf = [0u8; 2];
     msg_len_buf[0] = (buf.len() >> 8) as u8;
