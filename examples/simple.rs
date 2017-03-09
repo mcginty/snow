7e104a655d803364a496229c37d61464e12bccb4
Jake McGinty <me@jake.su>
2017-03-09 23:39:51 +0800

    splfy simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,7 +2,7 @@ extern crate clap;
 extern crate snow;
 
 use clap::App;
-use snow::*;
+use snow::{NoiseBuilder, NoiseParams, NoiseSessionState};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
@@ -15,7 +15,6 @@ fn main() {
     } else {
         run_client(params);
     }
-
     println!("all done!");
 }
 
@@ -23,24 +22,21 @@ fn run_server(params: NoiseParams) {
     let mut noise = NoiseBuilder::new(params).build_responder().unwrap();
     let mut buf = vec![0u8; 65535];
 
-    let listener = TcpListener::bind("127.0.0.1:9999").unwrap();
     println!("listening on 127.0.0.1:9999");
-    let (mut stream, _) = listener.accept().unwrap();
+    let (mut stream, _) = TcpListener::bind("127.0.0.1:9999").unwrap().accept().unwrap();
 
     // get first message from intiator
-    noise.read_message(&recv(&mut stream)[..], &mut buf).unwrap();
+    noise.read_message(&recv(&mut stream), &mut buf).unwrap();
 
     // respond, completing the handshake for NN
     let len = noise.write_message(&[0u8; 0], &mut buf).unwrap();
     send(&mut stream, &buf[..len]);
 
-    if noise.is_handshake_finished() {
-        println!("session established...");
-    }
-
+    // transition the state machine into transport mode
     let mut noise = noise.into_transport_mode().unwrap();
+    println!("session established...");
 
-    let len = noise.read_message(&recv(&mut stream)[..], &mut buf).unwrap();
+    let len = noise.read_message(&recv(&mut stream), &mut buf).unwrap();
     println!("client said: {}", String::from_utf8_lossy(&buf[..len]));
 }
 
@@ -55,13 +51,11 @@ fn run_client(params: NoiseParams) {
     send(&mut stream, &buf[..len]);
 
     // get response, completing the handshake for NN
-    let message = recv(&mut stream);
-    noise.read_message(&message[..], &mut buf).unwrap();
+    noise.read_message(&recv(&mut stream), &mut buf).unwrap();
 
-    if noise.is_handshake_finished() {
-        println!("session established...");
-    }
+    // transition the state machine into transport mode
     let mut noise = noise.into_transport_mode().unwrap();
+    println!("session established...");
 
     let len = noise.write_message("HACK THE PLANET".as_bytes(), &mut buf).unwrap();
     send(&mut stream, &buf[..len]);
@@ -71,7 +65,7 @@ fn run_client(params: NoiseParams) {
 fn recv(stream: &mut TcpStream) -> Vec<u8> {
     let mut msg_len_buf = [0u8; 2];
     stream.read_exact(&mut msg_len_buf).unwrap();
-    let mut msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
+    let msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
     let mut msg = vec![0u8; msg_len];
     stream.read_exact(&mut msg[..]).unwrap();
     msg
