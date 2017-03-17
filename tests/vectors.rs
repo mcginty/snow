7927a2049dc7dc6f75f32adafb4f07997c4a771b
Jake McGinty <me@jake.su>
2017-03-17 14:51:21 +0800

    Revert "ignore expensive tests by default, update travis"
    nevermind on that one, it turns out that also runs ignored
    documentation tests, which is both currently annoying
    and makes perfect sense.
    
    This reverts commit e919fb7b86afb2020a4638cbaaac9fa499ed324c.

--- tests/vectors.rs
+++ tests/vectors.rs
@@ -198,13 +198,11 @@ fn test_vectors_from_json(json: &str) {
 }
 
 #[test]
-#[ignore]
 fn test_vectors_noise_c_basic() {
     test_vectors_from_json(include_str!("vectors/noise-c-basic.txt"));
 }
 
 #[test]
-#[ignore]
 fn test_vectors_cacophony() {
     test_vectors_from_json(include_str!("vectors/cacophony.txt"));
 }
