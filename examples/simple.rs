89ad39730bf5e2c03cad16921fab64ec7fdf22c7
Jake McGinty <me@jake.su>
2017-05-31 12:24:31 +0800

    hack more planets

--- examples/simple.rs
+++ examples/simple.rs
@@ -11,7 +11,7 @@ extern crate snow;
 use clap::App;
 use snow::NoiseBuilder;
 use snow::params::NoiseParams;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -38,7 +38,7 @@ fn run_server(params: NoiseParams) {
     let (mut stream, _) = TcpListener::bind("127.0.0.1:9999").unwrap().accept().unwrap();
 
     // We're acting as the responder, so the first message is sent from the client.
-    noise.read_message(&recv(&mut stream), &mut buf).unwrap();
+    noise.read_message(&recv(&mut stream).unwrap(), &mut buf).unwrap();
 
     // Respond, thereby completing the handshake for NN, which is 1RTT.
     let len = noise.write_message(&[0u8; 0], &mut buf).unwrap();
@@ -47,8 +47,11 @@ fn run_server(params: NoiseParams) {
     // Transition the state machine into transport mode now that the handshake is complete.
     let mut noise = noise.into_transport_mode().unwrap();
 
-    let len = noise.read_message(&recv(&mut stream), &mut buf).unwrap();
-    println!("client said: {}", String::from_utf8_lossy(&buf[..len]));
+    while let Ok(msg) = recv(&mut stream) {
+        let len = noise.read_message(&msg, &mut buf).unwrap();
+        println!("client said: {}", String::from_utf8_lossy(&buf[..len]));
+    }
+    println!("connection closed.");
 }
 
 fn run_client(params: NoiseParams) {
@@ -66,33 +69,33 @@ fn run_client(params: NoiseParams) {
     send(&mut stream, &buf[..len]);
 
     // Receive response, thereby completing the handshake for NN
-    noise.read_message(&recv(&mut stream), &mut buf).unwrap();
+    noise.read_message(&recv(&mut stream).unwrap(), &mut buf).unwrap();
 
     // Transition the state machine into transport mode now that the handshake is complete.
     let mut noise = noise.into_transport_mode().unwrap();
     println!("session established...");
 
     // Get to the important business of sending secured data.
-    let len = noise.write_message(b"HACK THE PLANET", &mut buf).unwrap();
-    send(&mut stream, &buf[..len]);
+    for _ in 0..10 {
+        let len = noise.write_message(b"HACK THE PLANET", &mut buf).unwrap();
+        send(&mut stream, &buf[..len]);
+    }
     println!("notified server of intent to hack planet.");
 }
 
 /// Hyper-basic stream transport receiver. 16-bit BE size followed by payload.
-fn recv(stream: &mut TcpStream) -> Vec<u8> {
+fn recv(stream: &mut TcpStream) -> io::Result<Vec<u8>> {
     let mut msg_len_buf = [0u8; 2];
-    stream.read_exact(&mut msg_len_buf).unwrap();
+    stream.read_exact(&mut msg_len_buf)?;
     let msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
     let mut msg = vec![0u8; msg_len];
-    stream.read_exact(&mut msg[..]).unwrap();
-    msg
+    stream.read_exact(&mut msg[..])?;
+    Ok(msg)
 }
 
 /// Hyper-basic stream transport sender. 16-bit BE size followed by payload.
 fn send(stream: &mut TcpStream, buf: &[u8]) {
-    let mut msg_len_buf = [0u8; 2];
-    msg_len_buf[0] = (buf.len() >> 8) as u8;
-    msg_len_buf[1] = (buf.len() & 0xff) as u8;
+    let msg_len_buf = [(buf.len() >> 8) as u8, (buf.len() & 0xff) as u8];
     stream.write_all(&msg_len_buf).unwrap();
     stream.write_all(buf).unwrap();
 }
