9d1e41ffd5874b5d5860a9c9572bf0091670cc6b
Jake McGinty <me@jake.su>
2017-06-16 22:41:13 +0800

    generate and self-check multipsk vectors

--- tests/vectors.rs
+++ tests/vectors.rs
@@ -5,17 +5,34 @@ extern crate snow;
 #[macro_use] extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+extern crate rand;
 
 use serde::de::{self, Deserialize, Deserializer, Visitor, Unexpected};
+use serde::ser::{Serialize, Serializer};
 use std::ops::Deref;
 use hex::{FromHex, ToHex};
 use snow::{NoiseBuilder, Session};
 use snow::params::*;
+use snow::types::Dh;
+use snow::wrappers::crypto_wrapper::Dh25519;
+use snow::wrappers::rand_wrapper::RandomOs;
 use std::fmt;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
 
+#[derive(Clone)]
 struct HexBytes {
     original: String,
     payload: Vec<u8>,
+}
+
+impl From<Vec<u8>> for HexBytes {
+    fn from(payload: Vec<u8>) -> Self {
+        Self {
+            original: payload.to_hex(),
+            payload: payload,
+        }
+    }
 }
 
 impl Deref for HexBytes {
@@ -60,7 +77,15 @@ impl<'de> Deserialize<'de> for HexBytes {
     }
 }
 
-#[derive(Deserialize)]
+impl Serialize for HexBytes {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(&self.to_hex())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 struct TestMessage {
     payload: HexBytes,
     ciphertext: HexBytes,
@@ -72,28 +97,28 @@ impl fmt::Debug for TestMessage {
     }
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct TestVector {
-    name: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")] name: Option<String>,
     protocol_name: String,
-    hybrid: Option<String>,
-    fail: Option<bool>,
-    fallback: Option<bool>,
-    fallback_pattern: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")] hybrid: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")] fail: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")] fallback: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")] fallback_pattern: Option<String>,
     init_prologue: HexBytes,
-    init_psks: Option<Vec<HexBytes>>,
-    init_static: Option<HexBytes>,
-    init_ephemeral: Option<HexBytes>,
-    init_remote_static: Option<HexBytes>,
+    #[serde(skip_serializing_if="Option::is_none")] init_psks: Option<Vec<HexBytes>>,
+    #[serde(skip_serializing_if="Option::is_none")] init_static: Option<HexBytes>,
+    #[serde(skip_serializing_if="Option::is_none")] init_ephemeral: Option<HexBytes>,
+    #[serde(skip_serializing_if="Option::is_none")] init_remote_static: Option<HexBytes>,
     resp_prologue: HexBytes,
-    resp_psks: Option<Vec<HexBytes>>,
-    resp_static: Option<HexBytes>,
-    resp_ephemeral: Option<HexBytes>,
-    resp_remote_static: Option<HexBytes>,
+    #[serde(skip_serializing_if="Option::is_none")] resp_psks: Option<Vec<HexBytes>>,
+    #[serde(skip_serializing_if="Option::is_none")] resp_static: Option<HexBytes>,
+    #[serde(skip_serializing_if="Option::is_none")] resp_ephemeral: Option<HexBytes>,
+    #[serde(skip_serializing_if="Option::is_none")] resp_remote_static: Option<HexBytes>,
     messages: Vec<TestMessage>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct TestVectors {
     vectors: Vec<TestVector>,
 }
@@ -229,8 +254,141 @@ fn test_vectors_from_json(json: &str) {
 //    test_vectors_from_json(include_str!("vectors/noise-c-basic.txt"));
 //}
 
+fn random_vec(size: usize) -> Vec<u8> {
+    let mut v = Vec::with_capacity(size);
+    for _ in 0..size {
+        v.push(rand::random());
+    }
+    v
+}
+
+fn get_psks_count(params: &NoiseParams) -> usize {
+    let mut count = 0;
+    for modifier in &params.handshake.modifiers.list {
+        if let &HandshakeModifier::Psk(_) = modifier {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn generate_multipsk_vector(params: NoiseParams) -> TestVector {
+    let prologue = "There is no right and wrong. There's only fun and boring.".as_bytes().to_vec();
+    let mut rand = RandomOs::default();
+    let mut is = Dh25519::default();
+    let mut ie = Dh25519::default();
+    let mut rs = Dh25519::default();
+    let mut re = Dh25519::default();
+    is.generate(&mut rand);
+    ie.generate(&mut rand);
+    rs.generate(&mut rand);
+    re.generate(&mut rand);
+    let mut psks = vec![];
+    let mut psks_hex = vec![];
+
+    let mut init_b: NoiseBuilder = NoiseBuilder::new(params.clone());
+    let mut resp_b: NoiseBuilder = NoiseBuilder::new(params.clone());
+
+    for _ in 0..get_psks_count(&params) {
+        let v = random_vec(32);
+        psks_hex.push(v.clone().into());
+        psks.push(v);
+    }
+
+    let mut psk_index = 0;
+    for modifier in params.handshake.modifiers.list {
+        if let HandshakeModifier::Psk(n) = modifier {
+            init_b = init_b.psk(n, &psks[psk_index]);
+            resp_b = resp_b.psk(n, &psks[psk_index]);
+            psk_index += 1;
+        }
+    }
+    init_b = init_b.local_private_key(&is.privkey());
+    init_b = init_b.fixed_ephemeral_key_for_testing_only(&ie.privkey());
+    init_b = init_b.prologue(&prologue);
+    resp_b = resp_b.local_private_key(&rs.privkey());
+    resp_b = resp_b.fixed_ephemeral_key_for_testing_only(&re.privkey());
+    resp_b = resp_b.prologue(&prologue);
+
+    let mut init: Session = init_b.build_initiator().unwrap();
+    let mut resp: Session = resp_b.build_responder().unwrap();
+
+    let (mut ibuf, mut obuf) = ([0u8; 65535], [0u8; 65535]);
+    let mut messages = vec![];
+    while !(init.is_handshake_finished() && resp.is_handshake_finished()) {
+        let payload = random_vec(32);
+        let len = init.write_message(&payload, &mut ibuf).unwrap();
+        messages.push(TestMessage {
+            payload: payload.clone().into(),
+            ciphertext: ibuf[..len].to_vec().into(),
+        });
+        let _ = resp.read_message(&ibuf[..len], &mut obuf).unwrap();
+
+        if init.is_handshake_finished() && resp.is_handshake_finished() {
+            break;
+        }
+
+        let payload = random_vec(32);
+        let len = resp.write_message(&payload, &mut ibuf).unwrap();
+        messages.push(TestMessage {
+            payload: payload.clone().into(),
+            ciphertext: ibuf[..len].to_vec().into(),
+        });
+        let _ = init.read_message(&ibuf[..len], &mut obuf).unwrap();
+    }
+
+    TestVector {
+        name: None,
+        protocol_name: params.name,
+        hybrid: None,
+        fail: None,
+        fallback: None,
+        fallback_pattern: None,
+        init_prologue: prologue.clone().into(),
+        init_psks: Some(psks_hex.clone()),
+        init_static: Some(is.privkey().to_vec().into()),
+        init_ephemeral: Some(ie.privkey().to_vec().into()),
+        init_remote_static: None,
+        resp_prologue: prologue.clone().into(),
+        resp_psks: Some(psks_hex.clone()),
+        resp_static: Some(rs.privkey().to_vec().into()),
+        resp_ephemeral: Some(re.privkey().to_vec().into()),
+        resp_remote_static: None,
+        messages: messages,
+    }
+}
+
+fn generate_multipsk_vector_set() -> TestVectors {
+    let handshakes = vec!["XXpsk0+psk1", "XXpsk0+psk2", "XXpsk0+psk3", "XXpsk0+psk1+psk2+psk3"];
+    let ciphers = vec!["ChaChaPoly", "AESGCM"];
+    let hashes = vec!["BLAKE2s", "BLAKE2b", "SHA256", "SHA512"];
+
+    let mut vectors = vec![];
+
+    for handshake in &handshakes {
+        for cipher in &ciphers {
+            for hash in &hashes {
+                let protocol_name = format!("Noise_{}_25519_{}_{}", handshake, cipher, hash);
+                vectors.push(generate_multipsk_vector(protocol_name.parse().unwrap()));
+            }
+        }
+    }
+    TestVectors { vectors }
+}
+
 #[test]
 fn test_vectors_cacophony() {
     test_vectors_from_json(include_str!("vectors/cacophony.txt"));
 }
 
+#[test]
+fn test_vectors_snow_multipsk() {
+    let file = OpenOptions::new().write(true).create_new(true).open("tests/vectors/snow-multipsk.txt");
+    if let Ok(mut file) = file {
+        serde_json::to_writer_pretty(&mut file, &generate_multipsk_vector_set()).unwrap();
+    }
+    let mut file = File::open("tests/vectors/snow-multipsk.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    test_vectors_from_json(&contents);
+}
