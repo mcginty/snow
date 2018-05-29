3c7701388cf7e95f0e7c751ccd0f0c13d34dd35a
Jake McGinty <me@jake.su>
2018-05-28 20:07:20 -0500

    fix broken documentation on site

---
+++ docs/doc/implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["snow"] = [{text:"impl Fail for <a class=\"enum\" href=\"snow/enum.SnowError.html\" title=\"enum snow::SnowError\">SnowError</a>",synthetic:false,types:["snow::error::SnowError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
