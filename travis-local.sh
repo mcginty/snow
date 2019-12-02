3cbe2cfae45e5c45c6749454b53a7eab9ca8bfbf
Jake McGinty <me@jake.su>
2019-12-03 00:16:22 +0900

    Simplify CI tests.

--- travis-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! command -v yq >/dev/null 2>&1; then
-  echo "please install the \"yq\" binary (YAML wrapper for jq)"
-fi
-
-commands=$(yq -r '.script | join(" && ")' .travis.yml)
-echo "$commands"
-eval $commands
