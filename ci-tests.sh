ffbcb0a7e210a85f98e8cd3ed863726d6faf2eca
Jake McGinty <me@jake.su>
2019-12-03 21:59:45 +0900

    re-enable HFS tests for macOS/win

--- ci-tests.sh
+++ ci-tests.sh
@@ -8,11 +8,6 @@ cargo test $TARGET --no-default-features
 cargo test $TARGET --features "vector-tests"
 cargo test $TARGET --features "ring-resolver vector-tests"
 cargo test $TARGET --features "ring-accelerated vector-tests"
-
-if [[ "$OSTYPE" != "darwin"* && "$OSTYPE" != "msys" ]]; then
-  cargo test $TARGET --features "hfs pqclean_kyber1024 vector-tests"
-  cargo test $TARGET --features "ring-resolver hfs pqclean_kyber1024 vector-tests"
-else
-  echo "skipping HFS/PQClean tests, pending fix of https://github.com/rustpq/pqcrypto/issues/6"
-fi
+cargo test $TARGET --features "hfs pqclean_kyber1024 vector-tests"
+cargo test $TARGET --features "ring-resolver hfs pqclean_kyber1024 vector-tests"
 
