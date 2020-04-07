1dd0b1cc6214acb47a36e0226e4d27a127c2a1f6
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-04-07 15:45:58 +0200

    Fix tests for xchachapoly feature

--- ci-tests.sh
+++ ci-tests.sh
@@ -2,13 +2,15 @@
 set -e
 TARGET="$([ -n "$1" ] && echo "--target $1" || echo "")"
 
+COMMON_FEATURES="xchachapoly vector-tests"
+
 set -x
 cargo check --benches
 cargo test $TARGET --no-default-features
-cargo test $TARGET --features "vector-tests"
-cargo test $TARGET --features "ring-resolver vector-tests"
-cargo test $TARGET --features "ring-accelerated vector-tests"
-cargo test $TARGET --features "hfs pqclean_kyber1024 vector-tests"
-cargo test $TARGET --features "ring-resolver hfs pqclean_kyber1024 vector-tests"
-cargo test $TARGET --features "libsodium-resolver vector-tests"
-cargo test $TARGET --features "libsodium-accelerated vector-tests"
+cargo test $TARGET --features "$COMMON_FEATURES"
+cargo test $TARGET --features "ring-resolver $COMMON_FEATURES"
+cargo test $TARGET --features "ring-accelerated $COMMON_FEATURES"
+cargo test $TARGET --features "hfs pqclean_kyber1024 $COMMON_FEATURES"
+cargo test $TARGET --features "ring-resolver hfs pqclean_kyber1024 $COMMON_FEATURES"
+cargo test $TARGET --features "libsodium-resolver $COMMON_FEATURES"
+cargo test $TARGET --features "libsodium-accelerated $COMMON_FEATURES"
