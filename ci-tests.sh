2e5cf145ec511ea0b808efae20014fd93de93715
Jake McGinty <me@jake.su>
2022-02-02 01:32:40 -0600

    don't run hfs CI tests on windows

--- ci-tests.sh
+++ ci-tests.sh
@@ -10,7 +10,9 @@ cargo test $TARGET --no-default-features
 cargo test $TARGET --features "$COMMON_FEATURES"
 cargo test $TARGET --features "ring-resolver $COMMON_FEATURES"
 cargo test $TARGET --features "ring-accelerated $COMMON_FEATURES"
-cargo test $TARGET --features "hfs pqclean_kyber1024 $COMMON_FEATURES"
-cargo test $TARGET --features "ring-resolver hfs pqclean_kyber1024 $COMMON_FEATURES"
+if ! rustc -vV | grep 'host: .*windows' &> /dev/null; then
+    cargo test $TARGET --features "hfs pqclean_kyber1024 $COMMON_FEATURES"
+    cargo test $TARGET --features "ring-resolver hfs pqclean_kyber1024 $COMMON_FEATURES"
+fi
 cargo test $TARGET --features "libsodium-resolver $COMMON_FEATURES"
 cargo test $TARGET --features "libsodium-accelerated $COMMON_FEATURES"
