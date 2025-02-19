9eb74df49a3d42639237041101ee270328f30262
Jake McGinty <jake.mcginty@ditto.live>
2025-02-19 12:46:29 -0600

    ci-tests.sh: fail on clippy warnings

--- ci-tests.sh
+++ ci-tests.sh
@@ -2,11 +2,14 @@
 set -e
 TARGET="$([ -n "$1" ] && echo "--target $1" || echo "")"
 
-COMMON_FEATURES="p256 xchachapoly vector-tests"
+COMMON_FEATURES="use-p256 use-xchacha20poly1305 vector-tests"
+if ! rustc -vV | grep 'host: .*windows' &> /dev/null; then
+    COMMON_FEATURES="hfs use-pqcrypto-kyber1024 $COMMON_FEATURES"
+fi
 
 set -x
 cargo check --benches --tests --examples
-cargo clippy --features "$COMMON_FEATURES"
+cargo clippy --features "$COMMON_FEATURES" -- -Dwarnings
 cargo test $TARGET --no-default-features
 # Custom set of crypto without std
 cargo test $TARGET --no-default-features --features "default-resolver use-curve25519 use-blake2 use-chacha20poly1305"
@@ -14,9 +17,5 @@ cargo test $TARGET --no-default-features --features "default-resolver use-curve2
 cargo test $TARGET --no-default-features --features "default-resolver use-curve25519 use-sha2 use-chacha20poly1305"
 cargo test $TARGET --features "ring-resolver $COMMON_FEATURES"
 cargo test $TARGET --features "ring-accelerated $COMMON_FEATURES"
-if ! rustc -vV | grep 'host: .*windows' &> /dev/null; then
-    cargo test $TARGET --features "hfs use-pqcrypto-kyber1024 $COMMON_FEATURES"
-    cargo test $TARGET --features "ring-resolver hfs use-pqcrypto-kyber1024 $COMMON_FEATURES"
-fi
 cargo test $TARGET --features "libsodium-resolver $COMMON_FEATURES"
 cargo test $TARGET --features "libsodium-accelerated $COMMON_FEATURES"
