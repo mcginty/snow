e74230a97d927c0d1eb0efd37237e0ecbc2eb5be
Jake McGinty <me@jakebot.org>
2025-03-05 00:04:22 +0800

    only run clippy on MSRV when in CI mode

--- ci-tests.sh
+++ ci-tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 TARGET="$([ -n "$1" ] && echo "--target $1" || echo "")"
+MSRV="$(cargo metadata --no-deps | jq -r .packages[0].rust_version)"
+RUSTC_VERSION="$(rustc --version | cut -w -f2)"
 
 COMMON_FEATURES="use-p256 use-xchacha20poly1305 vector-tests"
 if ! rustc -vV | grep 'host: .*windows' &> /dev/null; then
@@ -9,7 +11,11 @@ fi
 
 set -x
 cargo check --benches --tests --examples
-cargo clippy --features "$COMMON_FEATURES" --tests --benches --examples -- -Dwarnings
+if [[ "$CI" != "true" || "$RUSTC_VERSION" == "$MSRV" ]]; then
+    cargo clippy --features "$COMMON_FEATURES" --tests --benches --examples -- -Dwarnings
+else
+    echo "skipping cargo clippy on non-MSRV CI run. MSRV: $MSRV, rustc: $RUSTC_VERSION"
+fi
 cargo test $TARGET --no-default-features
 # Custom set of crypto without std
 cargo test $TARGET --no-default-features --features "default-resolver use-curve25519 use-blake2 use-chacha20poly1305"
